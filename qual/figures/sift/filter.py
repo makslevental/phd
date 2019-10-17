d3f974c8dde1682676cc95afaedc02315663ab20
Maksim <maksim.levental@gmail.com>
2019-10-17 12:17:26 -0400

    started registration

--- qual/figures/sift/filter.py
+++ qual/figures/sift/filter.py
@@ -12,16 +12,14 @@ img = rgb2gray(img)
 
 plt.imshow(img, cmap=plt.cm.gray)
 plt.show()
-for j in [2, 1, 1 / 2, 1 / 4]:
-    img_rescale = rescale(img, j, anti_aliasing=True)
-    for i in range(0, 5):
-        new_filtered = gaussian(img_rescale, sqrt(2) ** i * 1.5)
-        if i > 0:
-            dog = new_filtered - old_filtered
-            skimage.io.imsave(f"dog_bertrand_sigma_{j}_{i}.png", dog)
-            plt.imshow(dog, cmap=plt.cm.gray)
-            plt.show()
-        skimage.io.imsave(f"bertrand_sigma_{j}_{i}.png", new_filtered)
-        plt.imshow(new_filtered, cmap=plt.cm.gray)
-        old_filtered = new_filtered
+for i in range(0, 6):
+    new_filtered = gaussian(img, sqrt(2) ** i * 1.5)
+    if i > 0:
+        dog = new_filtered - old_filtered
+        skimage.io.imsave(f"dog_bertrand_sigma_{i}.png", dog)
+        plt.imshow(dog, cmap=plt.cm.gray)
         plt.show()
+    skimage.io.imsave(f"bertrand_sigma_{i}.png", new_filtered)
+    plt.imshow(new_filtered, cmap=plt.cm.gray)
+    old_filtered = new_filtered
+    plt.show()
