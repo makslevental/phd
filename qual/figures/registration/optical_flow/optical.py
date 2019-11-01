5b7619bb5d5bc36bec6f4a1f1f71957455bc730c
maksim <maksim.levental@gmail.com>
2019-11-01 16:47:33 -0400

    correctly sized cubes

--- qual/figures/registration/optical_flow/optical.py
+++ qual/figures/registration/optical_flow/optical.py
@@ -7,13 +7,14 @@ from skimage.color import rgb2gray
 from skimage.data import stereo_motorcycle
 from skimage.feature import register_translation
 from skimage.registration import optical_flow_tvl1
+from skimage import io
 
-reference_image, moving_image, disp = stereo_motorcycle()
-reference_image = rgb2gray(reference_image)
-moving_image = rgb2gray(moving_image)
+# reference_image, moving_image, disp = stereo_motorcycle()
+# reference_image = rgb2gray(reference_image)
+# moving_image = rgb2gray(moving_image)
 
-reference_image = skimage.io.imread("cube1.png", as_gray=True)
-moving_image = skimage.io.imread("cube2.png", as_gray=True)
+reference_image = io.imread("cube1.png", as_gray=True)
+moving_image = io.imread("cube2.png", as_gray=True)
 dpi = 24
 px, py = reference_image.shape
 fig = pyplot.figure(figsize=(py / numpy.float(dpi), px / numpy.float(dpi)))
@@ -27,7 +28,7 @@ shift, error, diffphase = register_translation(reference_image, moving_image)
 print(shift)
 
 flow = optical_flow_tvl1(reference_image, moving_image)
-downscale = 10
+downscale = 20
 flow = skimage.measure.block_reduce(flow, (1, downscale, downscale), numpy.mean)
 
 h, w = flow[0].shape
@@ -39,7 +40,7 @@ M = np.hypot(U, V)
 q = ax.quiver(
     X, Y, U, V,
     transform=Affine2D().scale(downscale, downscale) + ax.transData,
-    width=.01,
+    width=.005,
     color="red"
 
 )
